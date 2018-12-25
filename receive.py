73852a9d0274ccf33e09657a3d3fa1b406d2f929
Meysam Azad <MeysamAzad81@yahoo.com>
2018-12-25 10:47:29 +0330

    add gitignore for python

---
+++ receive.py
@@ -0,0 +1,31 @@
+import pika
+import redis
+
+r = redis.Redis()
+r.set(0, 0)
+r.set(1, 1)
+
+def callback(ch, method, properties, body):
+    n = int(body)
+    print('received {}'.format(n))
+    response = fibo(n)
+    print('sent {}'.format(response))
+    ch.basic_publish(exchange='', routing_key=properties.reply_to,
+                     properties=pika.BasicProperties(correlation_id=properties.correlation_id),
+                     body=str(response))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+def fibo(n):
+    if r.get(n) is None:
+        for i in range(2, n + 1):
+            r.set(i, int(r.get(i-1)) + int(r.get(i-2)))
+    return int(r.get(n))
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+queue = 'rpc_queue'
+
+channel.queue_declare(queue=queue)
+channel.basic_qos(prefetch_count=1) # one message per receiver at a time
+channel.basic_consume(callback, queue=queue)
+channel.start_consuming()
---
+++ send.py
@@ -0,0 +1,35 @@
+import sys
+import pika
+import uuid
+
+class FibonacciRpcClient(object):
+    def __init__(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        self.channel = self.connection.channel()
+        result = self.channel.queue_declare(exclusive=True)
+        self.queue = result.method.queue
+
+        self.channel.basic_consume(self.callback, no_ack=True, queue=self.queue)
+
+    def callback(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def call(self, n):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(exchange='', routing_key=routing_key,
+                                   properties=pika.BasicProperties(reply_to=self.queue, correlation_id=self.corr_id,),
+                                   body=str(n))
+        while not self.response:
+            self.connection.process_data_events()
+
+        return int(self.response)
+
+message = sys.argv[1] if len(sys.argv) > 1 else "4"
+routing_key = 'rpc_queue'
+
+fibo_rpc = FibonacciRpcClient()
+print('send {}'.format(message))
+response = fibo_rpc.call(message)
+print('receive {}'.format(response))
